5c5c9435b189cc6abb25c4d6a18d3a9f30275283
Santiago Brun <brunsantiago@hotmail.com>
2023-01-03 19:53:46 -0300

    Https Prueba 1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import app from "./app.js";
 import { PORT } from "./config.js";
 
+//HTTP CONFIG
+
 app.listen(PORT);
 console.log(`Server on port http://localhost:${PORT}`);
 
 
-  //HTTPS CONFIG
+//HTTPS CONFIG
 
-  // import express from 'express';
-  // import fs from 'fs';
-  // import https from 'https';
-
-  // https.createServer({
-  //    cert: fs.readFileSync('localhost.pem'),
-  //    key: fs.readFileSync('localhost-key.pem')
-  //  },app).listen(PORT_HTTPS, function(){
-  // 	console.log('Servidor https corriendo en el puerto: '+PORT_HTTPS);
-  // });
+//import express from 'express';
+// import fs from 'fs';
+// import https from 'https';
+//
+// https.createServer({
+//    cert: fs.readFileSync('localhost.pem'),
+//    key: fs.readFileSync('localhost-key.pem')
+//  },app).listen(PORT_HTTPS, function(){
+// 	console.log('Servidor https corriendo en el puerto: '+PORT_HTTPS);
+// });
